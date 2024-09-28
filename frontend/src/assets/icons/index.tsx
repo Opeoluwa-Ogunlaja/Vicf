b0f1c29b1b3ce40c9bb1c7240511c13120e816e8
Opeoluwa-Ogunlaja <ogunlajafaith@gmail.com>
2024-09-28 13:58:56 +0100

    starter files for the project

---
+++ frontend/src/assets/icons/index.tsx
@@ -0,0 +1,6 @@
+/// <reference types="vite-plugin-svgr/client" />
+
+import AnnouncementIcon from './announcement.svg?react'
+import ClockRewindIcon from './clock-rewind.svg'
+
+export { AnnouncementIcon, ClockRewindIcon }
