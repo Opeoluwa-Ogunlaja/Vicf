01e65ec816873ee9ab660fdf6c4aa9502e8b6816
Opeoluwa-Ogunlaja <ogunlajafaith@gmail.com>
2025-05-05 18:13:57 +0100

    fix: update axiosInstance to use environment variable for baseURL; add Landing component with task queue functionality

---
+++ frontend/src/pages/landing/Landing.tsx
@@ -0,0 +1,22 @@
+import { TaskQueue } from '@/lib/taskQueue'
+import { wait } from '@/lib/utils/promiseUtils'
+import { useMemo } from 'react'
+
+const Landing = () => {
+  const queue = useMemo(() => {
+    const taskQueue = new TaskQueue(3)
+    return taskQueue
+  }, [])
+
+  const hello = () => wait(2000, 'hello').then(res => console.log(res, queue.paused))
+  return (
+    <div className="flex gap-4">
+      Landing
+      <button onClick={() => queue.runTask(hello)}>QueueTask</button>
+      <button onClick={() => queue.pause()}>Pause</button>
+      <button onClick={() => queue.resume()}>Resume</button>
+    </div>
+  )
+}
+
+export default Landing
