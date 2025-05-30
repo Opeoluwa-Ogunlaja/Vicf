5e3fcf696e2f2b5d5828f3478d34d73c29c4da7d
Opeoluwa-Ogunlaja <ogunlajafaith@gmail.com>
2025-05-30 13:45:52 +0100

    some kyn stuffs

--- backend/app.ts
+++ backend/app.ts
@@ -7,36 +7,11 @@ import contactsRouter from './routers/contactsRouter'
 import organisationsRouter from './routers/organisationsRouter'
 import path from 'path'
 import { nodeEnv } from './config'
-import fs from 'fs'
 
 const app = express()
 
-fs.readdir(__dirname, (e, val) => {
-  if (!e) console.log(val)
-})
-fs.readdir(path.join(__dirname, 'client'), (e, val) => {
-  if (!e) console.log(val)
-})
-
 app.use(cookieParser())
 app.use(express.json())
-
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:4173', 'https://vicf.vercel.app']
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true)
-
-      if (!allowedOrigins.includes(origin)) {
-        // const err = new Error('CORS wahala 😂😂')
-        // return callback(err, false)
-      }
-
-      return callback(null, true)
-    },
-    credentials: true
-  })
-)
 
 app.use('/users', userRouter)
 app.use('/contacts', contactsRouter)
